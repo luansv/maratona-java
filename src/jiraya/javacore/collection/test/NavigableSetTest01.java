cfc8250a3950895e1bc6663871456e75419d2bd5
Luan <luansilvf@gmail.com>
2025-01-30 21:56:28 -0300

    commit

--- src/jiraya/javacore/collection/test/NavigableSetTest01.java
+++ src/jiraya/javacore/collection/test/NavigableSetTest01.java
@@ -14,6 +14,14 @@ class SmartphoneBrandComparator implements Comparator<Smartphone> {
     }
 }
 
+class ArtistsStreamsComparator implements Comparator<Artists>{
+
+    @Override
+    public int compare(Artists o1, Artists o2) {
+        return Double.compare(o1.getStreams(), o2.getStreams());
+    }
+}
+
 public class NavigableSetTest01 {
     public static void main(String[] args) {
         NavigableSet<Smartphone> set = new TreeSet<>(new SmartphoneBrandComparator());
@@ -21,7 +29,7 @@ public class NavigableSetTest01 {
         set.add(smartphone);
         System.out.println(set);
 
-        NavigableSet<Artists> artists = new TreeSet<>();
+        NavigableSet<Artists> artists = new TreeSet<>(new ArtistsStreamsComparator());
         artists.add(new Artists(01L,"Arca", 7000.000, 1989));
         artists.add(new Artists(02L, "Bjork", 8000.00, 1956));
         artists.add(new Artists( 04L,"Angele",400.00,1996));
@@ -31,6 +39,18 @@ public class NavigableSetTest01 {
         for (Artists artist : artists) {
             System.out.println(artist);
         }
+
+        Artists example = new Artists(02L, "Ariana Grande", 10000.00, 1956);
+        System.out.println("-------------");
+        System.out.println(artists.lower(example));
+        System.out.println(artists.floor(example));
+        System.out.println(artists.higher(example));
+        System.out.println(artists.ceiling(example));
+
+
+        System.out.println("Size: "+artists.size());
+        System.out.println(artists.pollFirst());
+        System.out.println(artists.pollLast());
 
     }
 }
